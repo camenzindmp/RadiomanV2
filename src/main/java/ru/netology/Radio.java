d653823e8efb79cd43c9d3741f854533c9cb2019
Maksim <camenzindmp@gmail.com>
2020-05-31 12:18:57 +1000

    Methods Added

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,4 +1,97 @@
 package ru.netology;
 
 public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    //выставить номер радиостанции:
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    //кнопка next:
+    public void nextStationButton() {
+        if (currentStation == maxStation) {
+            this.currentStation = 0;
+        }
+        this.currentStation++;
+    }
+
+    //кнопка prev:
+    public void prevStationButton() {
+        if (currentStation == minStation) {
+            this.currentStation = 9;
+        }
+        this.currentStation++;
+    }
+
+    //увеличить громкость на 1:
+    public void volumePlusButton() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        this.currentVolume++;
+    }
+
+    //уменьшить громкость на 1:
+    public void volumeMinusButton() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        this.currentVolume--;
+    }
 }
